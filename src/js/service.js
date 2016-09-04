487b6d44a55408a4d6a36877e739700b89566d75
Selkin Vitaly <contact@selkinvitaly.ru>
2016-09-04 18:49:27 +0300

    updated sources

--- src/js/service.js
+++ src/js/service.js
@@ -1,74 +1,74 @@
 angular.module('angular-toasty').factory('toasty', ['$rootScope', 'toastyConfig', function($rootScope, toastyConfig) {
 
-	// Get the global config
-	var config = toastyConfig.config;
+  // Get the global config
+  var config = toastyConfig.config;
 
-	/**
-	 * Broadcast a new toasty item to the rootscope
-	 * @param  {object} options Individual toasty config overrides
-	 * @param  {string} type    Type of toasty; success, info, error etc.
-	 */
-	var toasty = function(options, type) {
+  /**
+   * Broadcast a new toasty item to the rootscope
+   * @param  {object} options Individual toasty config overrides
+   * @param  {string} type    Type of toasty; success, info, error etc.
+   */
+  var toasty = function(options, type) {
 
-		if (angular.isString(options) && options != '' || angular.isNumber(options)) {
-			options = {
-				title: options.toString()
-			};
-		}
+    if (angular.isString(options) && options != '' || angular.isNumber(options)) {
+      options = {
+        title: options.toString()
+      };
+    }
 
-		if (!options || !options.title && !options.msg) {
-			console.error('angular-toasty: No toast title or message specified!');
-		} else {
-			options.type = type || 'default';
-			$rootScope.$broadcast('toasty-new', {config: config, options: options});
-		}
-	};
+    if (!options || !options.title && !options.msg) {
+      console.error('angular-toasty: No toast title or message specified!');
+    } else {
+      options.type = type || 'default';
+      $rootScope.$broadcast('toasty-new', {config: config, options: options});
+    }
+  };
 
-	/**
-	 * Toasty types
-	 */
-	
-	toasty.default = function(options) { 
-		toasty(options);
-	};
+  /**
+   * Toasty types
+   */
 
-	toasty.info = function(options) { 
-		toasty(options, 'info');
-	};
+  toasty.default = function(options) {
+    toasty(options);
+  };
 
-	toasty.wait = function(options) { 
-		toasty(options, 'wait');
-	};
+  toasty.info = function(options) {
+    toasty(options, 'info');
+  };
 
-	toasty.success = function(options) { 
-		toasty(options, 'success');
-	};
+  toasty.wait = function(options) {
+    toasty(options, 'wait');
+  };
 
-	toasty.error = function(options) { 
-		toasty(options, 'error');
-	};
+  toasty.success = function(options) {
+    toasty(options, 'success');
+  };
 
-	toasty.warning = function(options) { 
-		toasty(options, 'warning');
-	};
+  toasty.error = function(options) {
+    toasty(options, 'error');
+  };
 
-	/**
-	 * Broadcast a clear event
-	 * @param {int} Optional ID of the toasty to clear
-	 */
+  toasty.warning = function(options) {
+    toasty(options, 'warning');
+  };
 
-	toasty.clear = function(id) {
-		$rootScope.$broadcast('toasty-clear', { id: id });
-	};
+  /**
+   * Broadcast a clear event
+   * @param {int} Optional ID of the toasty to clear
+   */
 
-	/**
-	 * Return the global config
-	 */
+  toasty.clear = function(id) {
+    $rootScope.$broadcast('toasty-clear', { id: id });
+  };
 
-	toasty.getGlobalConfig = function() {
-		return config;
-	};
+  /**
+   * Return the global config
+   */
 
-	return toasty;
+  toasty.getGlobalConfig = function() {
+    return config;
+  };
+
+  return toasty;
 
 }]);
